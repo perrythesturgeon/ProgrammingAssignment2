5cd7c204a36478c6c5d5cbc5d03b3657cbaa1b24
perrythesturgeon <phucchuminh@icloud.com>
2025-04-25 17:50:05 +0700

    This is my submission for Programming Assignment 2 of the course R Programming, by Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  # Argument of setter is assigned to x in the global environment
+  # Cache is set as empty
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  # Getter return x as it is
+  get <- function() x
+  
+  # Pass solve(x) into m in the global environment
+  setinverse <- function(solve) m <<- solve
+  
+  # Get the inverse from the cache(m)
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Get the cache(m), if not empty -> return the result
+  m <- x$getinverse()
+  if (!is.null(m)) {
+    message("Getting cached data")
+    return(m)
+  }
+  # Else getter get x, pass it into solve 
+  # and pass the whole thing into setinverse
+  # which put the result in m
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
